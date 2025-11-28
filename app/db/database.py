7df601e75c4b0cceec9d905bf48066f92ca4d686
Rahulsarma2003 <sharmarahul32578@gmail.com>
2025-11-29 04:26:17 +0530

    Remove __pycache__, venv, and vector DB folders

--- app/db/database.py
+++ app/db/database.py
@@ -8,18 +8,18 @@ load_dotenv();
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL");
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"sslmode": "require"},
-    pool_pre_ping=True
-)
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL,
+#     connect_args={"sslmode": "require"},
+#     pool_pre_ping=True
+# )
 
 # for running locally
 
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL,
-#     pool_pre_ping=True
-# )
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
