a1c25d6d96846cf67d16cafb700c5cc466fb9aad
Rahulsarma2003 <sharmarahul32578@gmail.com>
2025-12-05 20:58:17 +0530

    cors origin setup

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app.include_router(auth.router)
 #cors setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://pdf-query-frontend-kohl.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
